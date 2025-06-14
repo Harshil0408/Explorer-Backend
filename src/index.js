6755da8e9efb51079ba616d26039b933d94b98dd
Harshit Kanjariya <kanjariyaharshit@gmail.com>
2025-06-14 16:31:25 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-
-import express from 'express'
-const app = express();
-
 import 'dotenv/config'
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 connectDB()
     .then(() => {
@@ -14,20 +11,3 @@ connectDB()
     .catch((err) => {
         console.log("MONGODB CONNECTION ERROR", err)
     })
-
-// ; (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONOGODB_URI}/${DB_NAME}`)
-//         app.on("error", (error) => {
-//             console.log("ERRR", error)
-//             throw error
-//         })
-
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port ${process.env.PORT}`)
-//         })
-//     } catch (error) {
-//         console.error("ERROR", error)
-//         throw err
-//     }
-// })()
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,8 @@
 const asyncHandler = (reqHandler) => {
-    (req, res, next) => {
-        Promise.resolve(reqHandler(req, res, next))
-            .catch((err) => next(err));
+    return (req, res, next) => {
+        Promise.resolve(reqHandler(req, res, next)).catch
+            ((err) => next(err));
     }
 }
 
-export { asyncHandler }
+export { asyncHandler } 
