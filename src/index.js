2d00af5e4e7271c68a5efd9f8fa53675eb1ef550
Harshit Kanjariya <kanjariyaharshit@gmail.com>
2025-06-14 12:12:50 +0530

    Utils code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import 'dotenv/config'
 import connectDB from './db/index.js';
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB CONNECTION ERROR", err)
+    })
 
 // ; (async () => {
 //     try {
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,15 @@
+class APiResponse {
+    constructor(statusCode, data = null, message = "Success", success = true) {
+        this.statusCode = statusCode < 400;
+        this.data = data;
+        this.message = message;
+        this.success = success;
+
+        // Capture the stack trace if available
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor);
+        } else {
+            this.stack = new Error().stack;
+        }
+    }
+}
