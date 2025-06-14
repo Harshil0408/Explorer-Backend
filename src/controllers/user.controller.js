7916368fe0b955b1853410d7078c77a4ab536809
Harshit Kanjariya <kanjariyaharshit@gmail.com>
2025-06-14 18:49:06 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,59 @@
+import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { APiResponse } from "../utils/apiResponse.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok",
+    const { fullName, email, password, username } = req.body;
+
+    if (
+        [fullName, email, password, username].some(field => field.trim() === '')
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedUser = User.findOne({
+        $or: [
+            { username }, { email }
+        ]
     })
+
+    if (existedUser) {
+        throw new ApiError(409, "Username or email already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(500, "Failed to upload avatar to Cloudinary");
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage ? coverImage.url : "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = User.findById(user._id).select("-password -refreshToken")
+
+    if (!createdUser) {
+        throw new ApiError(500, "Failed to create user");
+    }
+
+    return res.status(201).json(
+        new APiResponse(200, createdUser, "User registered successfully")
+    )
+
 })
