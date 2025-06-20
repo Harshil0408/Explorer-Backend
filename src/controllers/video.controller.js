f13682a78381f9d258beccc584195522f0d1f214
Harshit Kanjariya <kanjariyaharshit@gmail.com>
2025-06-20 12:27:22 +0530

    feat: implement user notifications system with CRUD operations and notification preferences

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,29 @@
+import { asyncHandler } from '../utils/asyncHandler'
+
+const getAllVideos = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+})
+
+const publishVideo = asyncHandler(async (req, res) => {
+    const { title, description } = req.body
+})
+
+const updateVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+})
+
+const deleteVideo = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+})
+
+const togglePublishStatus = asyncHandler(async (req, res) => {
+    const { videoId } = req.params
+})
+
+export {
+    getAllVideos,
+    publishVideo,
+    updateVideo,
+    deleteVideo,
+    togglePublishStatus
+}
